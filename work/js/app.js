33f9e37d4cd047648946eaeae51f0ec97151132d
青柳香菜 <a.kana.4529@gmail.com>
2024-11-05 08:57:47 +0900

    2nd

--- work/js/app.js
+++ work/js/app.js
@@ -1,52 +1,41 @@
 // JavaScriptをつくる
 
-// API経由ででーたを取得　Json形式
-// https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json
-// 130000の都市コードは動的に変化させる
-
+//HTML内のid属性"get-weather"の要素を取得→wetherに代入
 let weather = document.getElementById("get-weather");
 
-//let city = document.getElementById("city-select");
-
+// API経由でデータを取得（Json形式）
+// 130000の都市コードは動的に変化させる→letで書いて${getCity}
+//ユーザーがクリックした際に、指定された都市の天気情報を取得する処理を実行
 weather.addEventListener("click", () => {
   const getCity = document.getElementById("city-select").value;
-  //let url1 = `https://www.jma.go.jp/bosai/forecast/data/overview_forecast/${getCity}.json`;
   let url = `https://www.jma.go.jp/bosai/forecast/data/forecast/${getCity}.json`;
 
-  //データを送る
-  //受け取る県を選択した県の値にする
-
-  fetch(url)
+  //fetchを使ってAPIにリクエストを送る→取得した情報をJavaScriptで利用.
+  //then()を使って、データの受信とその後の処理を順に行う
+  fetch(url) //データを送る
     .then(function (response) {
       return response.json();
     })
     .then(function (weather) {
-      console.log(weather);
-      // document.getElementById("publishingOffice").lastElementChild.textContent =
-      //   weather.publishingOffice;
-      // document.getElementById("reportDatetime").lastElementChild.textContent =
-      //   weather.reportDatetime;
-      // document.getElementById("targetArea").lastElementChild.textContent =
-      //   weather.targetArea;
-      // document.getElementById("headlineText").lastElementChild.textContent =
-      //   weather.headlineText;
-
-      // document.getElementById("text").lastElementChild.innerHTML =
-      //   weather.text.replace(/\n\n/g, "<br>");
-      let area = weather[0].timeSeries[0].areas[0];
-      console.log(area);
-      // 発表者と報告日時の情報を画面に書き出す
+      //受け取る県を選択した県の値にする
       document.getElementById("publishingOffice").lastElementChild.textContent =
         weather[0].publishingOffice;
       document.getElementById("reportDatetime").lastElementChild.textContent =
         weather[0].reportDatetime;
-      // 特定地域の情報を画面に書き出す
-      document.getElementById("todayHighTemperature").lastElementChild.Content =
-        area.area.tempsMax;
 
-      document.getElementById("todayLowTemperature").lastElementChild.Content =
-        area.area.tempsMin;
+      //取得したweatherデータから
+      const tempArea = weather[1].tempAverage.areas[0];
+      //(tempArea)の中の(areas)の中の(0)の中のmaxのデータを取り出す
+      document.getElementById(
+        "todayHighTemperature"
+      ).lastElementChild.textContent = tempArea.max;
+      //(tempArea)の中の(areas)の中の(0)の中のminのデータを取り出す
+      document.getElementById(
+        "todayLowTemperature"
+      ).lastElementChild.textContent = tempArea.min;
 
+      // (area) に関する天気情報を抽出
+      let area = weather[0].timeSeries[0].areas[0];
       document.getElementById("targetArea").lastElementChild.textContent =
         area.area.name;
       document.getElementById("today").lastElementChild.textContent =
